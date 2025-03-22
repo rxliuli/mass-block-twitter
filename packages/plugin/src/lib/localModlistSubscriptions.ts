a6543984f7dbcae8302f0515babd0a0aabf1eeca
Nulo <hi@nulo.in>
2025-03-22 12:06:28 -0300

    fix: use idb-keyval instead of localStorage

--- packages/plugin/src/lib/localModlistSubscriptions.ts
+++ packages/plugin/src/lib/localModlistSubscriptions.ts
@@ -1,26 +1,26 @@
 import { ModListSubscribeRequest } from "@mass-block-twitter/server";
-
-
+import { get, set, del, entries } from "idb-keyval";
 
 export async function setSubscription(modListId: string, action: ModListSubscribeRequest['action']) {
-  localStorage.setItem(`rxliuli:modlist:${modListId}:action`, action)
+  await set(`rxliuli:modlist:${modListId}:action`, action);
 }
+
 export async function removeSubscription(modListId: string) {
-  localStorage.removeItem(`rxliuli:modlist:${modListId}:action`)
+  await del(`rxliuli:modlist:${modListId}:action`);
 }
 
 export async function getSubscription(modListId: string): Promise<ModListSubscribeRequest['action'] | null> {
-  return localStorage.getItem(`rxliuli:modlist:${modListId}:action`) as ModListSubscribeRequest['action'] | null
+  return await get(`rxliuli:modlist:${modListId}:action`) as ModListSubscribeRequest['action'] | null;
 }
 
 export async function getAllSubscriptions(): Promise<Record<string, ModListSubscribeRequest['action']>> {
-  const keys = Object.keys(localStorage)
-  const modListIds = keys.filter((key) => key.startsWith('rxliuli:modlist:'))
-  const subscriptions = modListIds.reduce((acc, key) => {
-    const modListId = key.split(':')[2]
-    const action = localStorage.getItem(key) as ModListSubscribeRequest['action']
-    acc[modListId] = action
-    return acc
-  }, {} as Record<string, ModListSubscribeRequest['action']>)
-  return subscriptions
+  const allEntries = await entries();
+  const subscriptions = allEntries.reduce((acc, [key, value]) => {
+    if (typeof key === 'string' && key.startsWith('rxliuli:modlist:')) {
+      const modListId = key.split(':')[2];
+      acc[modListId] = value as ModListSubscribeRequest['action'];
+    }
+    return acc;
+  }, {} as Record<string, ModListSubscribeRequest['action']>);
+  return subscriptions;
 }

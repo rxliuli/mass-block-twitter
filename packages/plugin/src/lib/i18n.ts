4ebdca80aecc0d1362eb2eea514382465d5d2a67
Nulo <hola@nulo.lol>
2025-03-22 12:20:13 -0300

    Merge pull request #5 from catdevnull/i18n-es
    feat(i18n): add spanish support

--- packages/plugin/src/lib/i18n.ts
+++ packages/plugin/src/lib/i18n.ts
@@ -30,15 +30,20 @@ export const t = format
 type UnwrapStore<T> = T extends Readable<infer U> ? U : T
 export const tP: UnwrapStore<typeof format> = (...args) => get(format)(...args)
 
-const supportLanguages = ['en-US', 'zh-CN' /*, 'es' */]
+const supportLanguages = ['en-US', 'zh-CN', 'es']
 export function getLocaleLanguage(): 'en-US' | 'zh-CN' | 'es' {
   const settings = getSettings()
   if (settings.language && supportLanguages.includes(settings.language)) {
     return settings.language as 'en-US' | 'zh-CN' | 'es'
   }
-  const r = getLocaleFromNavigator()
+  let r = getLocaleFromNavigator()
+  console.log(r)
   if (r && supportLanguages.includes(r)) {
     return r as 'en-US' | 'zh-CN' | 'es'
+  }
+  r = r?.match(/\b([a-z]{2})\b/)?.[1] || null
+  if (r && supportLanguages.map(l => l.split('-')[0]).includes(r)) {
+    return supportLanguages.find(l => l.split('-')[0] === r) as 'en-US' | 'zh-CN' | 'es'
   }
   return 'en-US'
 }

3ab147cf26aff75d05007a492b68ab005eddfe25
rxliuli <rxliuli@gmail.com>
2025-03-30 17:42:37 +0800

    fix(plugin): match communities/<id>/* routes

--- packages/plugin/src/lib/components/logic/floating/utils/__tests__/getCommunityId.test.ts
+++ packages/plugin/src/lib/components/logic/floating/utils/__tests__/getCommunityId.test.ts
@@ -2,13 +2,15 @@ import { expect, it } from 'vitest'
 import { getCommunityId } from '../getCommunityId'
 
 it('getCommunityId', () => {
-  expect(
-    getCommunityId('https://x.com/i/communities/1900366536683987325/members'),
-  ).toBe('1900366536683987325')
-  expect(
-    getCommunityId('https://x.com/i/communities/1900366536683987325'),
-  ).toBe('1900366536683987325')
-  expect(
-    getCommunityId('https://x.com/i/communities/1900366536683987325/'),
-  ).toBe('1900366536683987325')
+  const list = [
+    'https://x.com/i/communities/1900366536683987325',
+    'https://x.com/i/communities/1900366536683987325/',
+    'https://x.com/i/communities/1900366536683987325/members',
+    'https://x.com/i/communities/1900366536683987325/members/',
+    'https://x.com/i/communities/1900366536683987325/moderators',
+    'https://x.com/i/communities/1900366536683987325/about',
+  ]
+  list.forEach((it) => {
+    expect(getCommunityId(it)).toBe('1900366536683987325')
+  })
 })
--- packages/plugin/src/lib/components/logic/floating/utils/getCommunityId.ts
+++ packages/plugin/src/lib/components/logic/floating/utils/getCommunityId.ts
@@ -3,7 +3,7 @@ export function getCommunityId(url: string) {
   // https://x.com/i/communities/1900366536683987325
   // https://x.com/i/communities/1900366536683987325/
   const match = new URL(url).pathname.match(
-    /^\/i\/communities\/(\d+)(\/members)?\/?$/,
+    /^\/i\/communities\/(\d+)(\/.*)?\/?$/,
   )
   if (!match) {
     return

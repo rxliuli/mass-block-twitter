5fea09cdaf3f49d375960c5f52edceb0b1ddd36d
rxliuli <rxliuli@gmail.com>
2025-04-16 16:22:53 +0800

    fix(plugin): restrict context menu to x.com domain

--- packages/plugin/src/entrypoints/background.ts
+++ packages/plugin/src/entrypoints/background.ts
@@ -6,6 +6,7 @@ export default defineBackground(() => {
       id: 'scan',
       title: 'Scan and Block',
       contexts: ['page'],
+      documentUrlPatterns: ['https://x.com/*', 'https://mobile.x.com/*'],
     })
   })
   browser.runtime.setUninstallURL(
